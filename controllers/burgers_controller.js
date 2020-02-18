5ad975a403905d950c27c7b10056683ce9c5ae6d
Bobby Odian <odianrobert@yahoo.com>
2020-02-17 22:53:40 -0500

    routes still not true, buttons not finctioning correctly

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,29 +12,24 @@ router.get("/", function(req, res) {
     });
   });
   
-  router.post("/api/burgers", function(req, res) {
-    burgers.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
+  router.post("/burgers", function(req, res) {
+    burger.create(["burger_name"], [req.body.burger_name], function(data) {
+      res.redirect("/burgers");
     });
   });
   
-  router.put("/api/burgers/:id", function(req, res) {
+  router.put("/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
   
     console.log("condition", condition);
   
-    burgers.update(
+    burger.update(
       {
         devoured: req.body.devoured
       },
       condition,
-      function(result) {
-        if (result.changedRows === 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        }
-        res.status(200).end();
+      function(data) {
+        res.redirect("/burgers");
   
       }
     );
