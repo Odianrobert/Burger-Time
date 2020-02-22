193ddfac760a084bf2ab253295625d76b65325df
Bobby Odian <odianrobert@yahoo.com>
2020-02-22 12:52:12 -0500

    added user info and password for JAWS

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'nuskkyrsgmn5rw8c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   port: 3306,
-  user: 'root',
-  password: '',
-  database: 'burgers_db'
+  user: 'untdlept4n3y45b5',
+  password: 'p18vik8gebp6t58g',
+  database: 'mjuv8u5g4jhx723u'
 })
 }
 connection.connect(function (err) {
